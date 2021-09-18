14b878972564cb924dec9340939edd14a016a3d4
Outer_Salem (Aero) <68174000+outersalemaero@users.noreply.github.com>
2021-09-18 13:18:58 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,16 +6,15 @@ let helpembed = new Discord.MessageEmbed()
     .setTitle("nOS Help")
     .setColor("#07C902")
     .addField("PREFIX:", process.env.BotPrefix)
-    .addField(";help", "Tells you some detailed information about the bot.")
-    .addField(";ping", "Gets the ping for the bot.")
+    .addField(":help", "Tells you some detailed information about the bot.")
+    .addField(":ping", "Gets the ping for the bot.")
     .addField("Admin Commands", "Only commands that admins can use!")
-    .addField(";say", "Makes the bot say whatever the user has requested.Runs bot code(Owner Only!)")
+    .addField(":say", "Makes the bot say whatever the user has requested. Runs bot code(Owner Only!)")
     .addField("Network Commands", "Only network members can use these!")
-    .addField(";restrict <user> <reason>", "Globally bans someone from the nOS module.")
-    .addField(";get <user>", "Gets a user from the DB")
-    .addField(";unrestrict <user>", " Unbans a user from the DB")
-    .addField(";eval", "Runs bot code(Owner Only!)")
-
+    .addField(":restrict <user> <reason>", "Globally bans someone from the HawkOS module.")
+    .addField(":get <user>", "Gets a user from the DB")
+    .addField(":unrestrict <user>", " Unbans a user from the DB")
+    .addField(":eval", "Runs bot code(Owner Only!)")
 
 module.exports = {
     name: 'help',
