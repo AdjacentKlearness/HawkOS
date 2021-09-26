c3bfa42d805afdc79b8bf93edae73a0441a58c3c
ezekieltem <82738698+ezekieltem@users.noreply.github.com>
2021-09-25 20:36:49 -0500

    Update setcredits.js

--- commands/setcredits.js
+++ commands/setcredits.js
@@ -1,54 +1,49 @@
 module.exports = {
-  name: 'restrict',
-  description: 'Ban command',
-  execute(suspect, reason, message, database) {
-
-    const nbx = require('noblox.js');
-    const Discord = require('discord.js');
-    async function exec() {
-      const userId = await nbx.getIdFromUsername(suspect).catch(e => "User not found");
-      if (userId !== "User not found") {
-        const playerName = await nbx.getUsernameFromId(userId)
-        var ref = database.ref("api/RobloxBanned/");
-        var usersRef = ref.child(userId);
-        var dbID = Math.floor((Math.random() * 9999999999999999) * 1);
-        usersRef.set({
-          Moderator: message.member.user.tag,
-          Reason: reason,
-          DatabaseID: dbID,
-          RobloxID: userId,
-          DiscordID: message.member.user.id,
-          Agent: 'Database',
-          SystemBanned: 'true'
-        });
-
-
-
-        const BanSuccess = new Discord.MessageEmbed()
-          .setColor('#07C902')
-          .setThumbnail("https://www.roblox.com/Thumbs/Avatar.ashx?x=500&y=500&Format=Png&Username=" + playerName + "")
-          .setDescription("**Return Ban Information for: **" + message.member.user.tag + "")
-          .addFields(
-            { name: 'Username', value: playerName },
-            { name: 'RobloxID', value: userId },
-            { name: 'DiscordID:', value: message.member.user.id },
-            { name: 'DatabaseID', value: dbID },
-            { name: 'Reason:', value: reason },
-            { name: 'Agent', value: 'Database' },
-            { name: 'System Banned', value: 'True' }
-
-
-          )
-
-        const BanError = new Discord.MessageEmbed()
-          .addFields(
-            { name: '**Error**', value: 'HTTP 400: Invalid  or Bad Request' }
-          )
-        return message.channel.send(`<@!${message.member.user.id}>`, BanSuccess)
-      } else {
-        return message.channel.send(`<@!${message.member.user.id}>`, BanError)
+    name: 'restrict',
+    description: 'Ban command',
+    execute(suspect, NewCount, message, database) {
+        const userId = await nbx.getIdFromUsername(suspect).catch(e => "User not found");
+        const playerName = await nbx.getUsernameFromId(UserId)
+  
+      const nbx = require('noblox.js');
+      const Discord = require('discord.js');
+      async function exec() {
+        if (userId !== "User not found") {
+          var ref = database.ref("api/Credits/"+userId+"/");
+          var oldCredits = 0
+          var Reference = firebase.database().ref("api/Credits/"+UserId+"/")
+              Reference.once("value").then(function (snapshot) {
+                  oldCredits = snapshot.val()
+              })
+          ref.set(NewCount);
+          var newCredits = 0
+          var Reference = firebase.database().ref("api/Credits/"+UserId+"/")
+              Reference.once("value").then(function (snapshot) {
+                  newCredits = snapshot.val()
+              })
+  
+  
+  
+          const BanSuccess = new Discord.MessageEmbed()
+            .setColor('#07C902')
+            .setThumbnail("https://www.roblox.com/Thumbs/Avatar.ashx?x=500&y=500&Format=Png&Username=" + playerName + "")
+            .setDescription("**Return Ban Information for: **" + message.member.user.tag + "")
+            .addFields(
+              { name: 'Username', value: playerName },
+              { name: 'UserID', value: userId },
+              { name: 'Old Credits:', value: oldCredits },
+              { name: 'New Credits', value: newCredits },
+            )
+  
+          const BanError = new Discord.MessageEmbed()
+            .addFields(
+              { name: '**Error**', value: 'HTTP 400: Invalid  or Bad Request' }
+            )
+          return message.channel.send(`<@!${message.member.user.id}>`, BanSuccess)
+        } else {
+          return message.channel.send(`<@!${message.member.user.id}>`, BanError)
+        }
       }
-    }
-    exec()
-  },
-};
+      exec()
+    },
+  };
