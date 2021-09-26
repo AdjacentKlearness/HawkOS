54214c6a370ea5524e153f65c5016ac2e3507293
ezekieltem <82738698+ezekieltem@users.noreply.github.com>
2021-09-25 21:02:30 -0500

    Update setcredits.js

--- commands/setcredits.js
+++ commands/setcredits.js
@@ -1,4 +1,4 @@
-/*module.exports = {
+module.exports = {
     name: 'restrict',
     description: 'Ban command',
     execute(suspect, NewCount, message, database) {
@@ -46,4 +46,4 @@
       }
       exec()
     },
-  };*/
+  };
