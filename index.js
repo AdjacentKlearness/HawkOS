c758171cc6a6c8d433434d89cb2c2dbaeaec8631
ezekieltem <82738698+ezekieltem@users.noreply.github.com>
2021-09-25 19:13:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,19 @@ client.on('message', message => {
   if (command == 'say') {
     client.commands.get('say').execute(message, args);
   }
-
+  if (command == 'credits') {
+    const suspect = args[0];
+    if (!message.member.roles.cache.some(role => role.name === banRole)) {
+      return message.channel.send(InvalidPermissions);
+    }
+    if (!suspect) {
+      return message.channel.send(
+        `<@!${message.member.user.id}>`,
+        InvalidArguments
+      );
+    }
+    client.commands.get('credits').execute(suspect, message, firebase);
+  }
   if (command === 'restrict') {
     const suspect = args[0];
     const reason = args.slice(1).join(' ');
