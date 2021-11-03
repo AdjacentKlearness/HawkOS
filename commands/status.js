8b33f79d9c13f3c8453a3172b6c4fa223311c21a
ezekieltem <82738698+ezekieltem@users.noreply.github.com>
2021-11-03 16:21:54 -0500

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,17 +1,22 @@
 const Discord = require('discord.js');
 
+const HelpEmbed = new Discord.MessageEmbed()
+.setTitle("Status Manager Help Menu")
+.addField("Argument 1 Inputs","get, set, help")
+.addField("get", "Returns the current status settings")
+.addField("set", "Sets the Status with the given settings")
+.addField("help", Shows this menu \n ----------------------------------------)
+.addField("Argument 2 Inputs", "playing, listening, watching")
+.addField("PLAYING","Playing STATUS")
+.addField("LISTENING","listening to STATUS")
+.addField("WATCHING",watching STATUS \n ----------------------------------------)
+.addField("Argument 3 Inputs", "What you want the status to be")
+
 module.exports = {
     name: 'status',
     description: 'Manages Status',
-    async execute(message, args) {
-        let pw = new Discord.MessageEmbed()
-            .setDescription(`:diamond_shape_with_a_dot_inside: Pinging...`);
-        message.channel.send(pw).then(msg => {
-            let p = new Discord.MessageEmbed()
-                .setDescription(`:ping_pong: Ping was successful. Latency is ${msg.createdTimestamp - message.createdTimestamp}ms.`)
-                .setColor('#07C902');
-            msg.edit(p)
-        })
-
+    async execute(message, args, client) {
+        message.channel.send(args[1])
+        if (args[1] === "help") return message.reply(HelpEmbed)
     },
 };
